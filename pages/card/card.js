6ab09d0fe8e32f15bad0c79950f98d8c54bc7a22
zhifeng.jiang <jiangzhifeng666@163.com>
2018-01-03 15:37:01 +0800

    增加充值

--- pages/card/card.js
+++ pages/card/card.js
@@ -1,34 +1,60 @@
-// pages/user/user.js
+// pages/card/card.js
 import fetch from '../../utils/fetch.js'
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    hidden: false,
-    frt: 0
+    cards:[
+      // {
+      //   url: '../../image/once.png',
+      //   score: 20,
+      //   fee: 2
+      // },
+      {
+        url: '../../image/month.png',
+        score: 50,
+        fee: 10
+      },
+      {
+        url: '../../image/quarter.png',
+        score: 100,
+        fee: 20
+      },
+      {
+        url: '../../image/year.png',
+        score: 500,
+        fee: 50
+      }
+    ],
+    selectId: 0
   },
 
-  recharge() {
-
-    wx.navigateTo({
-      url: '../card/card',
-    })
+  selectCard(e) {
+    this.setData({
+      selectId: e.target.dataset.index
+    });
   },
-  
-  queryfrt() {
 
+  buy(e) {
+    let fee = e.target.dataset.fee;
+    let score = e.target.dataset.score;
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    });
     fetch({
-      url: '/frt/query',
+      url: '/frt/invoke',
       //   baseUrl: "http://192.168.50.57:9888", 
       baseUrl: "https://store.lianlianchains.com",
       data: {
-        func: 'query',
+        func: 'transefer',
         ccId: '39304981a1b8d8a2dba6dc1b318267daa5c7ba4acfea4a99dab15e7ef9aee2c2',
-        usr: wx.getStorageSync('unionId'),
-        acc: wx.getStorageSync('unionId')
+        usr: 'frtpool',
+        acc: 'frtpool',
+        reacc: wx.getStorageSync('unionId'),
+        amt: score
       },
       noLoading: false,
       method: "GET",
@@ -36,15 +62,16 @@ Page({
       //  header: { 'content-type': 'application/json' }
     }).then(res => {
       console.log(res)
+      wx.hideLoading();
       if (res.code == '0') {
 
-        this.setData({
-          frt: res.result
+        wx.navigateBack({
+          url: '../user/user',
         })
-
       }
-    }).catch(err => {
 
+    }).catch(err => {
+      wx.hideLoading();
       console.log("出错了")
       wx.showToast({
         title: '网络繁忙'
@@ -53,75 +80,60 @@ Page({
     })
 
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-    let that = this;
-    wx.getUserInfo({
-      success: function (res) {
-        console.log(res.userInfo.nickName)
-        that.setData({
-          avatarUrl: res.userInfo.avatarUrl,
-          nickName: res.userInfo.nickName
-        })
-      },
-      fail: function (err) {
-        that.setData({
-          avatarUrl: '../../image/user.png'
-        })
-      }
-    });
-
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.queryfrt()
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
